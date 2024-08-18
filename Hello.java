a02f7758c382e93e8c7fde57e0bf75c5f0b6cad2
Circle02 <bar11mil@gmail.com>
2024-08-18 03:58:54 +0300

    modified files

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         for (int i=1; i<=10;i++)
         {
-            System.out.println("goodbye world" + i);
+            System.out.println("hello planet" + i);
         }
     }
 }
